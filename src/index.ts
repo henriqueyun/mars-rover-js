4219d32532499f8b661d79637ecc4c3dea679543
henriqueyun <henriqueyun@gmail.com>
2024-04-29 04:55:56 -0300

    chore: added dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import dotenv from "dotenv"
+dotenv.config()
 import { prepareElasticsearch, storeLogs } from "./es/client";
 import startRovers from "./startRovers";
 import CommandLineData from "./types/CommandLineData";
@@ -10,7 +12,7 @@ export const main = async (input: CommandLineData) => {
     console.log('The logs we\'re stored')
 }
 
-if (process.env.NODE_ENV?.toLowerCase() !== 'test') {
+if (process.env.NODE_ENV!.toLowerCase() !== 'test') {
     (async () => {
         await prepareElasticsearch()
         const input = CLIData
